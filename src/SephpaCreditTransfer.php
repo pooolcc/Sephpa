aa28d1e00a43d5fdf84705a60958af74b2ab7583
Mario <mario.vettorazzi@poool.cc>
2024-02-01 10:38:59 +0100

    added pain.001.001.09

--- src/SephpaCreditTransfer.php
+++ src/SephpaCreditTransfer.php
@@ -10,7 +10,9 @@
  */
 
 namespace AbcAeffchen\Sephpa;
+
 use AbcAeffchen\SepaUtilities\SepaUtilities;
+use AbcAeffchen\Sephpa\PaymentCollections\SepaCreditTransfer00100303;
 
 require_once __DIR__ . '/Sephpa.php';
 
@@ -21,6 +23,7 @@ class SephpaCreditTransfer extends Sephpa
 {
     // credit transfers versions
     public const SEPA_PAIN_001_001_03 = SepaUtilities::SEPA_PAIN_001_001_03;
+    public const SEPA_PAIN_001_001_09 = SepaUtilities::SEPA_PAIN_001_001_09;
     public const SEPA_PAIN_001_002_03 = SepaUtilities::SEPA_PAIN_001_002_03;
     public const SEPA_PAIN_001_003_03 = SepaUtilities::SEPA_PAIN_001_003_03;
 
@@ -28,6 +31,10 @@ class SephpaCreditTransfer extends Sephpa
      * @type string INITIAL_STRING_CT Initial sting for credit transfer pain.001.001.03
      */
     private const INITIAL_STRING_PAIN_001_001_03 = '<?xml version="1.0" encoding="UTF-8"?><Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.03" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:iso:std:iso:20022:tech:xsd:pain.001.001.03 pain.001.001.03.xsd"></Document>';
+    /**
+     * @type string INITIAL_STRING_CT Initial sting for credit transfer pain.001.001.03
+     */
+    private const INITIAL_STRING_PAIN_001_001_09 = '<?xml version="1.0" encoding="UTF-8"?><Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.09" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:iso:std:iso:20022:tech:xsd:pain.001.001.09 pain.001.001.09.xsd"></Document>';
     /**
      * @type string INITIAL_STRING_CT Initial sting for credit transfer pain.001.002.03
      */
@@ -37,29 +44,31 @@ class SephpaCreditTransfer extends Sephpa
      */
     private const INITIAL_STRING_PAIN_001_003_03 = '<?xml version="1.0" encoding="UTF-8"?><Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.003.03" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:iso:std:iso:20022:tech:xsd:pain.001.003.03 pain.001.003.03.xsd"></Document>';
 
-    private const VERSIONS = [self::SEPA_PAIN_001_001_03 => ['class'   => '00100103',
-                                                             'initStr' => self::INITIAL_STRING_PAIN_001_001_03],
-                              self::SEPA_PAIN_001_002_03 => ['class'   => '00100203',
-                                                             'initStr' => self::INITIAL_STRING_PAIN_001_002_03],
-                              self::SEPA_PAIN_001_003_03 => ['class'   => '00100303',
-                                                             'initStr' => self::INITIAL_STRING_PAIN_001_003_03]];
+    private const VERSIONS = [self::SEPA_PAIN_001_001_03 => ['class' => '00100103',
+        'initStr' => self::INITIAL_STRING_PAIN_001_001_03],
+        self::SEPA_PAIN_001_001_09 => ['class' => '00100109',
+            'initStr' => self::INITIAL_STRING_PAIN_001_001_09],
+        self::SEPA_PAIN_001_002_03 => ['class' => '00100203',
+            'initStr' => self::INITIAL_STRING_PAIN_001_002_03],
+        self::SEPA_PAIN_001_003_03 => ['class' => '00100303',
+            'initStr' => self::INITIAL_STRING_PAIN_001_003_03]];
 
     /**
      * Creates a SepaXmlFile object and sets the head data
      *
-     * @param string   $initgPty   The name of the initiating party
-     * @param string   $msgId      The unique id of the file (max. 70 characters)
-     * @param int      $version    Sets the type and version of the sepa file. Use the SEPA_PAIN_*
+     * @param string $initgPty The name of the initiating party
+     * @param string $msgId The unique id of the file (max. 70 characters)
+     * @param int $version Sets the type and version of the sepa file. Use the SEPA_PAIN_*
      *                             constants
-     * @param string[] $orgId      It is not recommended to use this at all. If you have to use
+     * @param string[] $orgId It is not recommended to use this at all. If you have to use
      *                             this, the standard only allows one of the two. If you provide
      *                             both, options, both will be included in the SEPA file. So
      *                             only use this if you know what you do. Available keys:
      *                             - `id`: An Identifier of the organisation.
      *                             - `bob`: A BIC or BEI that identifies the organisation.
      *                             - `scheme_name`: max. 35 characters.
-     * @param string   $initgPtyId An ID of the initiating party (max. 35 characters)
-     * @param bool     $checkAndSanitize
+     * @param string $initgPtyId An ID of the initiating party (max. 35 characters)
+     * @param bool $checkAndSanitize
      * @throws SephpaInputException
      */
     public function __construct($initgPty, $msgId, $version, array $orgId = [], $initgPtyId = null, $checkAndSanitize = true)
@@ -68,7 +77,7 @@ class SephpaCreditTransfer extends Sephpa
 
         $this->paymentType = 'CstmrCdtTrfInitn';
 
-        if(!isset(self::VERSIONS[$version]))
+        if (!isset(self::VERSIONS[$version]))
             throw new SephpaInputException('You choose an invalid SEPA file version. Please use the SEPA_PAIN_001_* constants.');
 
         $this->version = $version;
@@ -81,9 +90,10 @@ class SephpaCreditTransfer extends Sephpa
      * @param array $collectionInfo @see \Sephpa\SepaCreditTransfer*::addPayment() for details.
      * @return PaymentCollections\SepaPaymentCollection
      */
-    public function addCollection(array $collectionInfo) : PaymentCollections\SepaPaymentCollection
+    public function addCollection(array $collectionInfo): PaymentCollections\SepaPaymentCollection
     {
         $class = 'AbcAeffchen\Sephpa\PaymentCollections\SepaCreditTransfer00' . $this->version;
+
         $this->paymentCollections[] = new $class($collectionInfo, $this->checkAndSanitize, $this->sanitizeFlags);
         return end($this->paymentCollections);
     }
---
+++ src/payment-collections/SepaCreditTransfer00100109.php
@@ -0,0 +1,225 @@
+<?php
+/*
+ * Sephpa
+ *
+ * @license   GNU LGPL v3.0 - For details have a look at the LICENSE file
+ * @copyright ©2021 Alexander Schickedanz
+ * @link      https://github.com/AbcAeffchen/Sephpa
+ *
+ * @author  Alexander Schickedanz <abcaeffchen@gmail.com>
+ */
+
+namespace AbcAeffchen\Sephpa\PaymentCollections;
+use AbcAeffchen\SepaUtilities\SepaUtilities;
+use AbcAeffchen\Sephpa\SephpaInputException;
+
+/**
+ * Manages credit transfers
+ */
+class SepaCreditTransfer00100109 extends SepaCreditTransferCollection
+{
+    /**
+     * @type int VERSION The SEPA file version of this collection
+     */
+    const VERSION = SepaUtilities::SEPA_PAIN_001_001_09;
+
+    /**
+     * @param mixed[] $transferInfo needed keys: 'pmtInfId', 'dbtr', 'iban';
+     *                              optional keys: 'ccy', 'btchBookg', 'ctgyPurp', 'reqdExctnDt',
+     *                              'ultmtDbtr', 'bic', 'pstlAdr'
+     * @param bool $checkAndSanitize           All inputs will be checked and sanitized before creating the
+     *                              collection. If you check the inputs yourself you can set this
+     *                              to false.
+     * @param int     $flags        The flags used for sanitizing
+     * @throws SephpaInputException
+     */
+    public function __construct(array $transferInfo, $checkAndSanitize = true, $flags = 0)
+    {
+        $this->today = (int) date('Ymd');
+        $this->checkAndSanitize = $checkAndSanitize;
+        $this->sanitizeFlags = $flags;
+
+        if(!SepaUtilities::checkRequiredCollectionKeys($transferInfo, self::VERSION) )
+            throw new SephpaInputException('The values of \'pmtInfId\', \'dbtr\', \'iban\' must not be empty.');
+
+        // backward compatibility / deprecated
+        if(isset($transferInfo['dbtrPstlAdr']) && !isset($transferInfo['pstlAdr']))
+        {
+            $transferInfo['pstlAdr'] = $transferInfo['dbtrPstlAdr'];
+        }
+
+        if($this->checkAndSanitize)
+        {
+            // All fields contain valid information?
+            $checkResult = SepaUtilities::checkAndSanitizeAll($transferInfo, $this->sanitizeFlags, ['allowEmptyBic' => true]);
+
+            if($checkResult !== true)
+                throw new SephpaInputException('The values of ' . $checkResult . ' are invalid.');
+
+            $this->dbtrIban = $transferInfo['iban'];
+
+            if(!empty($transferInfo['bic']) && !SepaUtilities::crossCheckIbanBic($transferInfo['iban'],$transferInfo['bic']))
+                throw new SephpaInputException('IBAN and BIC do not belong to each other.');
+        }
+
+        $this->transferInfo = $transferInfo;
+    }
+
+    /**
+     * Adds a payment to the payment collection
+     *
+     * @param mixed[] $paymentInfo needed keys: 'pmtId', 'instdAmt', 'iban', 'bic', 'cdtr';
+     *                             optional keys: 'ultmtCdrt', 'purp', 'rmtInf', 'ultmtDbtr', 'ultmtDbtrId'
+     * @throws SephpaInputException
+     * @return void
+     */
+    public function addPayment(array $paymentInfo)
+    {
+        if($this->checkAndSanitize)
+        {
+            if(!SepaUtilities::checkRequiredPaymentKeys($paymentInfo, self::VERSION) )
+                throw new SephpaInputException('One of the required inputs \'pmtId\', \'instdAmt\', \'iban\', \'cdtr\' is missing.');
+
+            $bicRequired = (!SepaUtilities::isEEATransaction($this->dbtrIban,$paymentInfo['iban']));
+
+            $checkResult = SepaUtilities::checkAndSanitizeAll($paymentInfo, $this->sanitizeFlags,
+                                                              ['allowEmptyBic' => !$bicRequired]);
+
+            if($checkResult !== true)
+                throw new SephpaInputException('The values of ' . $checkResult . ' are invalid.');
+
+            // IBAN and BIC can belong to each other?
+            if(!empty($paymentInfo['bic']) && !SepaUtilities::crossCheckIbanBic($paymentInfo['iban'],$paymentInfo['bic']))
+                throw new SephpaInputException('IBAN and BIC do not belong to each other.');
+        }
+
+        $this->payments[] = $paymentInfo;
+    }
+    
+    /**
+     * Generates the xml for the collection using generatePaymentXml
+     * @param \SimpleXMLElement $pmtInf The PmtInf-Child of the xml object
+     * @return void
+     */
+    public function generateCollectionXml(\SimpleXMLElement $pmtInf)
+    {
+        $ccy = ( empty( $this->transferInfo['ccy'] ) )
+            ? self::CCY
+            : $this->transferInfo['ccy'];
+
+        $datetime    = new \DateTime();
+        $reqdExctnDt = ( isset( $this->transferInfo['reqdExctnDt'] ) )
+            ? $this->transferInfo['reqdExctnDt'] : $datetime->format('Y-m-d');
+
+        $pmtInf->addChild('PmtInfId', $this->transferInfo['pmtInfId']);
+        $pmtInf->addChild('PmtMtd', 'TRF');
+        if( !empty( $this->transferInfo['btchBookg'] ) )
+            $pmtInf->addChild('BtchBookg', $this->transferInfo['btchBookg']);
+        $pmtInf->addChild('NbOfTxs', $this->getNumberOfTransactions());
+        $pmtInf->addChild('CtrlSum', sprintf("%01.2F", $this->getCtrlSum()));
+
+        $pmtTpInf = $pmtInf->addChild('PmtTpInf');
+        $pmtTpInf->addChild('InstrPrty', 'NORM');
+        $pmtTpInf->addChild('SvcLvl')->addChild('Cd', 'SEPA');
+        if( !empty( $this->transferInfo['ctgyPurp'] ) )
+            $pmtTpInf->addChild('CtgyPurp')->addChild('Cd', $this->transferInfo['ctgyPurp']);
+
+        $pmtInf->addChild('ReqdExctnDt');
+        $pmtInf->ReqdExctnDt->addChild('Dt', $reqdExctnDt);
+        $pmtInf->addChild('Dbtr')->addChild('Nm', $this->transferInfo['dbtr']);
+
+        if(isset($this->transferInfo['pstlAdr']))
+        {
+            $pstlAdr = $pmtInf->Dbtr->addChild('PstlAdr');
+
+            if(isset($this->transferInfo['pstlAdr']['ctry']))
+                $pstlAdr->addChild('Ctry', $this->transferInfo['pstlAdr']['ctry']);
+
+            if(isset($this->transferInfo['pstlAdr']['adrLine']))
+            {
+                foreach(is_array($this->transferInfo['pstlAdr']['adrLine'])
+                            ? $this->transferInfo['pstlAdr']['adrLine']
+                            : [$this->transferInfo['pstlAdr']['adrLine']] as $adrLine)
+                    $pstlAdr->addChild('AdrLine', $adrLine);
+            }
+        }
+
+        $dbtrAcct = $pmtInf->addChild('DbtrAcct');
+        $dbtrAcct->addChild('Id')->addChild('IBAN', $this->transferInfo['iban']);
+        $dbtrAcct->addChild('Ccy', $ccy);
+
+        if( !empty( $this->transferInfo['bic'] ) )
+            $pmtInf->addChild('DbtrAgt')->addChild('FinInstnId')
+                   ->addChild('BICFI', $this->transferInfo['bic']);
+        else
+            $pmtInf->addChild('DbtrAgt')->addChild('FinInstnId')->addChild('Othr')
+                   ->addChild('Id', 'NOTPROVIDED');
+
+        if( isset( $this->transferInfo['ultmtDbtr'] ) )
+            $pmtInf->addChild('UltmtDbtr')->addChild('Nm', $this->transferInfo['ultmtDbtr']);
+
+        $pmtInf->addChild('ChrgBr', 'SLEV');
+
+        foreach($this->payments as $payment)
+        {
+            $cdtTrfTxInf = $pmtInf->addChild('CdtTrfTxInf');
+            $this->generatePaymentXml($cdtTrfTxInf, $payment, $ccy);
+        }
+    }
+    
+    /**
+     * generates the xml for a single payment
+     * @param \SimpleXMLElement $cdtTrfTxInf
+     * @param mixed[] $payment one of the payments in $this->payments
+     * @param string $ccy currency
+     * @return void
+     */
+    private function generatePaymentXml(\SimpleXMLElement $cdtTrfTxInf, $payment, $ccy)
+    {
+        $cdtTrfTxInf->addChild('PmtId')->addChild('EndToEndId', $payment['pmtId']);
+        $cdtTrfTxInf->addChild('Amt')->addChild('InstdAmt', sprintf("%01.2F", $payment['instdAmt']))
+                    ->addAttribute('Ccy', $ccy);
+
+        if( isset( $payment['ultmtDbtr'] ) ||  isset( $payment['ultmtDbtrId'] ) ){
+            $cdtTrfTxInf->addChild('UltmtDbtr');
+
+            if( isset( $payment['ultmtDbtr'] )  )
+                $cdtTrfTxInf->UltmtDbtr->addChild('Nm', $payment['ultmtDbtr']);
+
+            if( isset( $payment['ultmtDbtrId'] )  )
+                $cdtTrfTxInf->UltmtDbtr->addChild('Id')->addChild('OrgId')->addChild('Othr')->addChild('Id', $payment['ultmtDbtrId']);
+        }
+
+        if( !empty( $payment['bic'] ) )
+            $cdtTrfTxInf->addChild('CdtrAgt')->addChild('FinInstnId')
+                        ->addChild('BICFI', $payment['bic']);
+
+        $cdtTrfTxInf->addChild('Cdtr')->addChild('Nm', $payment['cdtr']);
+
+        if(isset($payment['pstlAdr']))
+        {
+            $pstlAdr = $cdtTrfTxInf->Cdtr->addChild('PstlAdr');
+
+            if(isset($payment['pstlAdr']['ctry']))
+                $pstlAdr->addChild('Ctry', $payment['pstlAdr']['ctry']);
+
+            if(isset($payment['pstlAdr']['adrLine']))
+            {
+                foreach(is_array($payment['pstlAdr']['adrLine'])
+                            ? $payment['pstlAdr']['adrLine']
+                            : [$payment['pstlAdr']['adrLine']] as $adrLine)
+                    $pstlAdr->addChild('AdrLine', $adrLine);
+            }
+        }
+
+        $cdtTrfTxInf->addChild('CdtrAcct')->addChild('Id')->addChild('IBAN', $payment['iban']);
+
+        if( isset( $payment['ultmtCdtr'] ) )
+            $cdtTrfTxInf->addChild('UltmtCdtr')->addChild('Nm', $payment['ultmtCdtr']);
+        if( isset( $payment['purp'] ) )
+            $cdtTrfTxInf->addChild('Purp')->addChild('Cd', $payment['purp']);
+        if( isset( $payment['rmtInf'] ) )
+            $cdtTrfTxInf->addChild('RmtInf')->addChild('Ustrd', $payment['rmtInf']);
+    }
+
+}
